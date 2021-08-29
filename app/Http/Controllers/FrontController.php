b07ca000e6b9c647cb103dcbd839ce28a23ad857
Error Man <toonteamm@gmail.com>
2021-08-29 10:33:01 +0700

    css

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -13,7 +13,7 @@ class FrontController extends Controller
      */
     public function home()
     {
-        return view('welcome');
+        return view('index');
     }
 
     public function LinkIncome()
---
+++ resources/views/index.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container" style="background-image: url('/images/bg-blur.jpg'); background-attachment:fixed;">
+    <div class="row">
+        <div class="col-lg-1 col-md-2"></div>
+        <div class="col-lg-10 col-md-8">
+            <span style="font-size: 50px;">487ATK</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-1 col-md-2"></div>
+        <div class="col-lg-3 col-md-2">
+            <img src="/images/เว็บพนันออนไลน์-300x300.jpg" style="width: 100%;">
+        </div>
+        <div class="col-lg-7 col-md-8">
+            <div class="gb-container gb-container-8426a28c" style="margin-bottom: 100px;">
+                <div class="form-group htext" style="margin: 10px;">
+                    เว็บไซต์บริการเดิมพันออนไลน์แห่งใหม่ที่ก้าวเข้ามาให้บริการผู้เล่นไทยอย่างเต็มตัว เรามีความพร้อมและศักยภาพระดับมืออาชีพเพื่อให้ตอบโจทย์ผู้เล่นไทยได้มากที่สุด ยิ่งไปกว่านั้น 487ATK เป็นเว็บไซต์ระดับแนวหน้าที่เปิดให้บริการในทั่วเอเชีย ทางเรามีนโยบายด้านความรับผิดชอบต่อผู้เล่นที่เป็นไปตามแบบฉบับของสากล ความมั่นคงที่ท่านต้องการมี ให้คุณแล้วที่นี่ หากท่านต้องการเชียร์ทีมที่ใช่ กีฬาที่ชอบ 487ATK มอบสิ่งเหล่านี้ให้ท่านอย่างเต็มรูปแบบ การบริการด้วยระบบที่ทันสมัย ไม่ว่าท่านจะอยู่ที่ใดก็สามารถร่วมเดิมพันกับเราได้ เซียนคาสิโนห้ามพลาดกับเกมส์คาสิโนที่หลากหลาย หมวดสล็อตต่างๆ ที่ถูกใจคนไทย ให้ท่านได้เลือกสนุกและรับรางวัลได้แบบไม่อั้น หากคุณกำลังมองหาเจ้ามือรับเดิมพันออนไลน์น์ดีๆ ล่ะก็ ให้ 487ATK เป็นหนึ่งในตัวเลือกของท่านได้เลือกใช้บริการ ทางเราได้ขนมาจัดให้แล้วแบบครบสูตร เหลือแค่คุณเข้าไปลองสมัครสมาชิก 487ATK และเล่นด้วยตัวเองเท่านั้น!!
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-1 col-md-2"></div>
+        <div class="col-lg-10 col-md-8">
+            <iframe id="score108" width="100%" height="520" frameborder="0" scrolling="no" src="https://www.score108.com/soccer/scoreAPI"></iframe>
+        </div>
+    </div>
+    <div class="row" style="margin-top: 100px;margin-bottom: 100px;">
+        <div class="col-lg-1 col-md-2"></div>
+        <div class="col-lg-10 col-md-2">
+            <div class="text-center">ตารางคะแนน</div>
+            <div>
+                <iframe id="score108" style="width: 100%;height: 500px;" frameborder="0" scrolling="no" src="https://www.score108.com/soccer/standingAPI"></iframe>
+            </div>
+        </div>
+    </div>
+    <div class="row text-center" style="margin-top: 100px;margin-bottom: 100px;">
+        <div class="col-lg-1 col-md-2"></div>
+        <div class="col-lg-10 col-md-2">
+            <section>
+                <div class="rt-container">
+                    <div class="col-rt-12">
+                        <div class="Scriptcontent">
+                            <!-- Start slider -->
+                            <section class="auto-slider">
+                                <div id="slider">
+                                    <figure>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-amb.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-gss.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-joker.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-pg.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-pp-casino.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-pretty.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-rt.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-sa.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-sexy.png" style="margin: 10px;width: 400px;"></a>
+                                        <a href="https://sport.autoplay.cloud/?prefix=QlRU" target="_blacnk"><img src="/images/games-launcher/play-button-simpleplay.png" style="margin: 10px;width: 400px;"></a>
+                                        <!-- <img src="/images/games-launcher/play-button-wm.png" style="margin: 10px;width: 400px;">
+                                        <img src="/images/games-launcher/play-button-xin.png" style="margin: 10px;width: 400px;"> -->
+                                    </figure>
+                                    <!-- <figure>
+                                        <img src="/images/games-launcher/play-button-amb.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-gss.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-joker.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-pg.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-pp-casino.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-pretty.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-rt.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-sa.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-sexy.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-simpleplay.png" style="margin: 10px;width: 400px;">
+                                    </figure> -->
+                                    <!-- <figure>
+                                        <img src="/images/games-launcher/play-button-wm.png" style="margin: 10px;width: 400px;">
+                                    </figure>
+                                    <figure>
+                                        <img src="/images/games-launcher/play-button-xin.png" style="margin: 10px;width: 400px;">
+                                    </figure> -->
+                                    <!-- <div class="indicator"></div> -->
+                                </div>
+                            </section>
+                            <!-- end slider -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <div>
+        <center>
+            <img src="images/ครบจบในเว็ปเดียว.gif" alt="" width="786" height="256">
+        </center>
+    </div>
+    <div class="row">
+        <div class="col-lg-1 col-md-2"></div>
+        <div class="col-lg-10 col-md-8">
+            <span style="font-size: 50px;">ระบบฝากออโต้ ไม่ต้องรอ</span>
+            <div class="gb-container gb-container-8426a28c" style="margin-bottom: 100px;">
+                <div class="form-group htext" style="margin: 10px; margin-bottom: 50px;">
+                    ฝากระบบออโต้ ท่านสามารถทำการฝากเงิน ผ่านหน้าเว็บของเราได้ด้วยระบบอัตโนมัติ ที่รวดเร็ว ใช้เวลาไม่เกิน 30 วินาที ก็สามารถทำรายการต่างๆได้ ไม่จำเป็นต้องเข้าแชทแจ้งฝากกับพนักงาน ไม่ต้องรอพนักงานตรวจสอบยอดเงิน ฝากได้ไม่มีขั้นต่ำ ถอนขั้นต่ำ100บาท ให้คุณไม่พลาดทุกจังหวะการเดิมพัน และไม่ต้องห่วงเรื่องความปลอดภัย เพราะเราใช้ระบบ ฝากออโต้ ที่ได้มาตรฐานระดับเดียวกับที่ธนาคารชั้นนำใช้ คุณจึงมั่นใจได้ว่า ทุกยอดที่ท่านเล่นได้ จะกลับมาเข้ากระเป๋าท่านอย่างแน่นอน เพราะเราคือเว็บคาสิโนออนไลน์ที่เป็นที่นิยมของผู้ใช้งานกว่าล้านคน เว็บคาสิโนออนไลน์อันดับต้น ๆ ของเอเชีย เล่นได้จ่ายจริง ไม่มีโกงแน่นอน
+                    <center><img src="/images/รองรับทุกธนาคาร.png" style="width: 70%;"></center>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix" style="margin-top: 50px;"></div>
+    <div class="row">
+        <div class="col-lg-2"></div>
+        <div class="col-lg-8 col-md-6 text-center">
+
+        </div>
+    </div>
+    <div class="clearfix" style="margin-bottom: 100px;"></div>
+</div>
+@endsection
