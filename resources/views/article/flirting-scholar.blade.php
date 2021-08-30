693c94352d9f0309abab370c7bf66e3bc9cbb084
Error Man <toonteamm@gmail.com>
2021-08-30 17:57:25 +0700

    addd

--- resources/views/article/flirting-scholar.blade.php
+++ resources/views/article/flirting-scholar.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="text-center" style="margin-bottom: 50px;">
-            <img src="images/article/Flirting Scholar/เกมสล็อต-Flirting-Scholar-530x530-min.jpg.png" style="width: 100%;">
+            <img src="images/article/Flirting Scholar/Flirting Scholar.jpg">
         </div>
         <div style="margin-bottom: 50px;">
             <div class="row">
